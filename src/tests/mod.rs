bdf73edcf3787bb109745622fd60fca23fc61f02
Felix Schütt <felix.schuett@maps4print.com>
2017-05-06 11:49:30 +0200

    Fixed unit tests after API change

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -4,9 +4,10 @@ use std::fs::File;
 #[test]
 fn test_simple_empty_file() {
 
-    let (doc, _, _) = PdfDocument::new(PdfPage::new(247.0, 210.0,PdfLayer::new("Layer 1")), 
-                                       "Hello World PDF!", "superprogram_v1.1");
+    let (doc, _, _) = PdfDocument::new(
+                                      PdfPage::new(247.0, 210.0, 
+                                          PdfLayer::new("Layer 1")), 
+                                  "PDF_Document_title");
 
     doc.save(&mut File::create("test_simple_empty_file.pdf").unwrap()).unwrap();
-
 }
