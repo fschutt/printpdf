d2288de11c0b539d45eb91903a0721fbb30d39b4
Felix Schütt <felix.schuett@maps4print.com>
2017-05-16 19:35:51 +0200

    Fixed unit tests

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -4,7 +4,11 @@ use std::fs::File;
 #[test]
 fn test_simple_empty_file() {
 
-    let (mut doc, page1, layer1) = PdfDocument::new("PDF_Document_title", 247.0, 210.0, "Layer 1");
-    doc.save(&mut File::create("test_simple_empty_file.pdf").unwrap()).unwrap();
+  /*  let (mut doc, page1, layer1) = PdfDocument::new("PDF_Document_title", 247.0, 210.0, "Layer 1");
+    doc.save(&mut File::create("test_simple_empty_file.pdf").unwrap()).unwrap(); */
     
+  use std::sync::Arc;
+
+  let (doc, page1, layer1) = PdfDocument::new("PDF_Document_title", 247.0, 210.0, "Layer 1");
+  Arc::try_unwrap(doc).unwrap().into_inner().unwrap().save(&mut File::create("test_simple_empty_file.pdf").unwrap()).unwrap();
 }
