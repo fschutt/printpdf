51d45c8208e11c6318a1b9f01753777b82ab6331
sizeru <gaafts@gmail.com>
2023-07-05 14:25:41 -0400

    Add comment warning against needless cloning

--- src/svg.rs
+++ src/svg.rs
@@ -6,7 +6,11 @@ use lopdf::{Object, Stream};
 use std::{error, fmt};
 
 /// SVG - wrapper around an `XObject` to allow for more
-/// control within the library
+/// control within the library. 
+/// When creating placing multiple copies of the same SVGs
+/// on the same layer, it is better to use the
+/// `into_xobject` method to get a reference, rather than a
+/// clone
 #[derive(Debug, Clone)]
 pub struct Svg {
     /// The PDF document, converted from SVG using svg2pdf
