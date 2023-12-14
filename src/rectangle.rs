d3de3f068ce57ede39a415faea1efada7667d580
Edoardo Morandi <morandidodo@gmail.com>
2023-12-14 10:42:54 +0100

    feat!: impl adding rectangles in pdf layers
    With this implementation is also possible to use rectangles to clip
    other paths.

--- src/rectangle.rs
+++ src/rectangle.rs
@@ -1,6 +1,9 @@
 //! Utilities for rectangle paths.
 
-use crate::{Mm, Point};
+use crate::{
+    path::{PaintMode, WindingOrder},
+    Mm, Point, OP_PATH_CONST_RECT, OP_PATH_PAINT_END, OP_PATH_PAINT_STROKE,
+};
 
 /// A helper struct to insert rectangular shapes into a PDF.
 ///
@@ -13,6 +16,10 @@ pub struct Rect {
     /// Position of the upper right point of the rectangle, relative to the bottom left corner of
     /// the PDF page in pt.
     pub ur: Point,
+    /// The paint mode of the rectangle.
+    pub mode: PaintMode,
+    /// The path-painting/clipping path operator.
+    pub winding: WindingOrder,
 }
 
 impl Rect {
@@ -30,6 +37,58 @@ impl Rect {
                 x: urx.into(),
                 y: ury.into(),
             },
+            mode: PaintMode::default(),
+            winding: WindingOrder::default(),
+        }
+    }
+
+    /// Returns a new `Rect` with the specified `mode`.
+    #[inline]
+    #[must_use]
+    pub fn with_mode(mut self, mode: PaintMode) -> Self {
+        self.mode = mode;
+        self
+    }
+
+    /// Returns a new `Rect` with the specified `winding`.
+    #[inline]
+    #[must_use]
+    pub fn with_winding(mut self, winding: WindingOrder) -> Self {
+        self.winding = winding;
+        self
+    }
+
+    /// Transform the `Rect` into a `Vec` of PDF [`Operation`]s.
+    ///
+    /// [`Operation`]: lopdf::content::Operation
+    #[must_use]
+    pub fn into_stream_op(self) -> Vec<lopdf::content::Operation> {
+        use lopdf::content::Operation;
+
+        let width = self.ur.x - self.ll.x;
+        let height = self.ur.y - self.ll.y;
+
+        let rect_op = Operation::new(
+            OP_PATH_CONST_RECT,
+            vec![
+                self.ll.x.into(),
+                self.ll.y.into(),
+                width.into(),
+                height.into(),
+            ],
+        );
+
+        let paint_op = match self.mode {
+            PaintMode::Clip => Operation::new(self.winding.get_clip_op(), vec![]),
+            PaintMode::Fill => Operation::new(self.winding.get_fill_op(), vec![]),
+            PaintMode::Stroke => Operation::new(OP_PATH_PAINT_STROKE, vec![]),
+            PaintMode::FillStroke => Operation::new(self.winding.get_fill_stroke_op(), vec![]),
+        };
+
+        if matches!(self.mode, PaintMode::Clip) {
+            vec![rect_op, paint_op, Operation::new(OP_PATH_PAINT_END, vec![])]
+        } else {
+            vec![rect_op, paint_op]
         }
     }
 }
